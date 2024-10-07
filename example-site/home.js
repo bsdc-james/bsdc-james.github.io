cc79e5eb6f05bfc879721b2be39efea626b88429
JP30263113 <30263113@bsdc.ac.uk>
2024-10-07 16:26:17 +0100

    hub

--- example-site/home.js
+++ example-site/home.js
@@ -3,5 +3,30 @@ function homePageClickHandler(){
     window.location.replace("home.html");
 }
 
+function aboutPageClickHandler(){
+    console.log("Detected click on 'navBarAbout'")
+    window.location.replace("aboutus.html");
+}
+
+function servicesPageClickHandler(){
+    console.log("Detected click on 'navBarServices'")
+    window.location.replace("services.html");
+}
+
+function contactPageClickHandler(){
+    console.log("Detected click on 'navBarContact'")
+    window.location.replace("contactus.html");
+}
+
 var navBarLogo = document.getElementById("navBarLogo")
 navBarLogo.addEventListener('click', homePageClickHandler)
+
+var navBarAbout = document.getElementById("navBarAbout")
+navBarAbout.addEventListener('click', aboutPageClickHandler)
+
+var navBarServices = document.getElementById("navBarServices")
+navBarServices.addEventListener('click', servicesPageClickHandler)
+
+var navBarContact = document.getElementById("navBarContact")
+navBarContact.addEventListener('click', contactPageClickHandler)
+
--- example-site/services.js
+++ example-site/services.js
@@ -3,5 +3,30 @@ function homePageClickHandler(){
     window.location.replace("home.html");
 }
 
+function aboutPageClickHandler(){
+    console.log("Detected click on 'navBarAbout'")
+    window.location.replace("aboutus.html");
+}
+
+function servicesPageClickHandler(){
+    console.log("Detected click on 'navBarServices'")
+    window.location.replace("services.html");
+}
+
+function contactPageClickHandler(){
+    console.log("Detected click on 'navBarContact'")
+    window.location.replace("contactus.html");
+}
+
 var navBarLogo = document.getElementById("navBarLogo")
 navBarLogo.addEventListener('click', homePageClickHandler)
+
+var navBarAbout = document.getElementById("navBarAbout")
+navBarLogo.addEventListener('click', aboutPageClickHandler)     
+
+var navBarLogo = document.getElementById("navBarServices")
+navBarLogo.addEventListener('click', servicesPageClickHandler)
+
+var navBarLogo = document.getElementById("navBarContact")
+navBarLogo.addEventListener('click', contactPageClickHandler)
+
