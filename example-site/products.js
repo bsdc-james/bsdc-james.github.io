6a84e108dfc83d19c72625c4a4aaa57815ecd3aa
JP30263113 <30263113@bsdc.ac.uk>
2024-10-07 15:52:31 +0100

    draje9iofjsdoicmawoifhjvdsfpokmijqupodm uojsnviojgoierucj kpesbvoum.iusmcoksnburvojedoewmcovueoi

--- example-site/products.js
+++
@@ -1,7 +0,0 @@
-function homePageClickHandler(){
-    console.log("Detected click on 'navBarLogo'")
-    window.location.replace("home.html");
-}
-
-var navBarLogo = document.getElementById("navBarLogo")
-navBarLogo.addEventListener('click', homePageClickHandler)
