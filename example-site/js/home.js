a5a9410235799c6b41c1efc95baa8dc6db3a8e41
bsdc-james <30263113@bsdc.ac.uk>
2024-10-25 09:29:52 +0100

    idk bar update ig

--- example-site/js/home.js
+++ example-site/js/home.js
@@ -3,20 +3,20 @@ function homePageClickHandler(){
     window.location.replace("home.html");
 }
 
-function aboutPageClickHandler(){
-    console.log("Detected click on 'navBarAbout'")
-    window.location.replace("aboutus.html");
-}
+// function aboutPageClickHandler(){
+//     console.log("Detected click on 'navBarAbout'")
+//     window.location.replace("aboutus.html");
+// }
 
 function servicesPageClickHandler(){
     console.log("Detected click on 'navBarServices'")
     window.location.replace("services.html");
 }
 
-function contactPageClickHandler(){
-    console.log("Detected click on 'navBarContact'")
-    window.location.replace("contactus.html");
-}
+// function contactPageClickHandler(){
+//     console.log("Detected click on 'navBarContact'")
+//     window.location.replace("contactus.html");
+// }
 
 function hiddenBoxClickHandler(){
     console.log("Detected click on 'hiddenBox'")
@@ -28,14 +28,14 @@ console.log("i hate js get this shit off the face of the earth")
 var navBarLogo = document.getElementById("navBarLogo")
 navBarLogo.addEventListener('click', homePageClickHandler)
 
-var navBarAbout = document.getElementById("navBarAbout")
-navBarAbout.addEventListener('click', aboutPageClickHandler)
+// var navBarAbout = document.getElementById("navBarAbout")
+// navBarAbout.addEventListener('click', aboutPageClickHandler)
 
 var navBarServices = document.getElementById("navBarServices")
 navBarServices.addEventListener('click', servicesPageClickHandler)
 
-var navBarContact = document.getElementById("navBarContact")
-navBarContact.addEventListener('click', contactPageClickHandler)
+// var navBarContact = document.getElementById("navBarContact")
+// navBarContact.addEventListener('click', contactPageClickHandler)
 
 var hiddenBox = document.getElementById("hiddenBox")
 hiddenBox.addEventListener('click', hiddenBoxClickHandler)
