696d4ac795b992a4cc7e881f4ad1a086222454ba
30263113 <30263113@bsdc.ac.uk>
2024-11-13 16:04:13 +0000

    im honestly about to hang myself

---
+++ bobthebuilder/app.js
@@ -0,0 +1,10 @@
+console.log("I HATE JAVASCRIPT IT SHOULD BE WIPED OFF THFACE OF THE REATHC REAHGGGGG")
+
+
+const bob = document.getElementById("box")
+console.log(bob.innerHTML)
+
+function bobsaysbye(){
+    if bob.innerHTML = `<h1 id="text" class="text">bob is going to cry<br>why is bob the builder in my college class</h1>
+    <img src="/images/depressobob.png" alt="">;`
+}
